3cfa9c38173406d1ad9a8b4670eaa74d5b793dde
VKaramanovski <56731909+VKaramanovski@users.noreply.github.com>
2019-11-15 13:51:44 +0100

    Add files via upload

---
+++ ADSS_HW3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.X86;
+
+namespace ADSS_HW3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            int a = 15;
+            int b = 3;
+
+            Console.WriteLine(BasicOp('+', a, b));
+            Console.WriteLine(BasicOp('-', a, b));
+            Console.WriteLine(BasicOp('*', a, b));
+            Console.WriteLine(BasicOp('/', a, b));
+
+
+            Console.WriteLine(ConvertToBin(1564, 22));
+
+
+		}
+
+		static string ConvertToBin(int a, int b)
+		{
+			int numToConvert = a + b;
+			string binary = Convert.ToString(numToConvert, 2);
+			return binary;
+		}
+
+		static int BasicOp(char c, int a, int b)
+		{
+			var operation = 0;
+
+			switch (c)
+			{
+				case '+':
+					operation = a + b;
+					return operation;
+
+				case '-':
+					operation = a - b;
+					return operation;
+
+				case '*':
+					operation = a * b;
+					return operation;
+
+				case '/':
+					operation = a / b;
+					return operation;
+				default:
+					Console.WriteLine("Wrong operation parameter!");
+					return operation;
+			}
+		}
+	}
+}
